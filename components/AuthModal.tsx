3ea504c7a2eb8fddd728369afa5b7df697342ed6
Ansh <42040155+anshnen@users.noreply.github.com>
2023-09-04 19:31:19 +0530

    Update AuthModal.tsx

--- components/AuthModal.tsx
+++ components/AuthModal.tsx
@@ -42,7 +42,7 @@ const AuthModal = () => {
     >
       <Auth
         supabaseClient={supabaseClient}
-        //providers={['github']}
+        providers={['github']}
         magicLink={true}
         appearance={{
           theme: ThemeSupa,

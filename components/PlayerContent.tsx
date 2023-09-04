6e5cda59a175d4ee58940dbe2fb5f34a90789e9e
Ansh <42040155+anshnen@users.noreply.github.com>
2023-09-04 19:38:59 +0530

    Update PlayerContent.tsx

--- components/PlayerContent.tsx
+++ components/PlayerContent.tsx
@@ -1,6 +1,8 @@
 "use client";
 
+// @ts-ignore
 import useSound from "use-sound";
+
 import { useEffect, useState } from "react";
 import { BsPauseFill, BsPlayFill } from "react-icons/bs";
 import { HiSpeakerWave, HiSpeakerXMark } from "react-icons/hi2";
